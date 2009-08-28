36af259601b8b21186897f2efbcc899d60c534b0
Michael D'Aamour <mdamour@pentaho.com>
2009-08-28 17:58:24 +0000

    PPP-2182 we should not be showing the .prpt extention in the solution browser

--- src/org/pentaho/reporting/platform/plugin/ReportFileMetaDataProvider.java
+++ src/org/pentaho/reporting/platform/plugin/ReportFileMetaDataProvider.java
@@ -1,22 +1,12 @@
 package org.pentaho.reporting.platform.plugin;
 
 import java.io.InputStream;
-import java.util.HashMap;
 
 import org.pentaho.platform.api.engine.IFileInfo;
 import org.pentaho.platform.api.engine.ILogger;
 import org.pentaho.platform.api.engine.ISolutionFile;
 import org.pentaho.platform.api.engine.SolutionFileMetaAdapter;
-import org.pentaho.platform.api.repository.ISolutionRepository;
 import org.pentaho.platform.engine.core.solution.FileInfo;
-import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
-import org.pentaho.reporting.engine.classic.core.AttributeNames;
-import org.pentaho.reporting.libraries.docbundle.DocumentBundle;
-import org.pentaho.reporting.libraries.resourceloader.FactoryParameterKey;
-import org.pentaho.reporting.libraries.resourceloader.Resource;
-import org.pentaho.reporting.libraries.resourceloader.ResourceKey;
-import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
 
 public class ReportFileMetaDataProvider extends SolutionFileMetaAdapter
 {
@@ -35,7 +25,11 @@ public class ReportFileMetaDataProvider extends SolutionFileMetaAdapter
     try
     {
       fileInfo = new FileInfo();
-      fileInfo.setTitle(solutionFile.getFileName());
+      String title = solutionFile.getFileName();
+      if (solutionFile.getFileName().endsWith(".prpt")) {
+        title = title.substring(0,title.indexOf(".prpt"));
+      }
+      fileInfo.setTitle(title);
       fileInfo.setAuthor("");
       fileInfo.setDescription(solutionFile.getFullPath());
     } catch (Exception e)
