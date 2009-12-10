2d1f7378d3c02500073320b26afb3c855c42467f
Thomas Morgner <tmorgner@pentaho.com>
2009-12-10 17:07:44 +0000

    Backporting the changes for PRD-2342 (Need way to define the date-format on parameter-input) and PRD-2339 (Date-Input behaves badly if invalid data is fed into it, crashing the whole report-viewer with it)

--- src/org/pentaho/reporting/platform/plugin/gwt/client/DateParameterUI.java
+++ src/org/pentaho/reporting/platform/plugin/gwt/client/DateParameterUI.java
@@ -38,13 +38,36 @@ public class DateParameterUI extends SimplePanel
 
   }
 
+  private Date parseDate(final String text)
+  {
+    try
+    {
+      return new Date(Long.parseLong(text));
+    }
+    catch (Exception e)
+    {
+      // invalid number as well
+    }
+    try
+    {
+      final DateTimeFormat format = DateTimeFormat.getFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+      return format.parse(text);
+    }
+    catch (Exception e)
+    {
+      // invalid date string ..
+    }
+    return new Date();
+  }
+
   public DateParameterUI(final ParameterControllerPanel controller, final List<String> parameterSelections, final Element parameterElement)
   {
     // selectionsList should only have 1 date
     Date date = new Date();
     if (parameterSelections.size() > 0)
     {
-      date = new Date(Long.parseLong(parameterSelections.get(0)));
+      final String paramAsText = parameterSelections.get(0);
+      date = parseDate(paramAsText);
     }
     else
     {
@@ -54,11 +77,25 @@ public class DateParameterUI extends SimplePanel
       parameterSelections.add("" + date.getTime()); //$NON-NLS-1$
     }
 
-    DefaultFormat format = new DefaultFormat(DateTimeFormat.getLongDateFormat());
-    DateBox datePicker = new DateBox(new PentahoDatePicker(), date, format);
+    final DefaultFormat format = new DefaultFormat(createFormat(parameterElement.getAttribute("data-format")));
+    final DateBox datePicker = new DateBox(new PentahoDatePicker(), date, format);
 
     datePicker.addValueChangeHandler(new DateParameterSelectionHandler(parameterSelections, controller));
     setWidget(datePicker);
   }
 
+  private DateTimeFormat createFormat(final String format)
+  {
+    if (format != null)
+    try
+    {
+      return DateTimeFormat.getFormat(format);
+    }
+    catch (Exception e)
+    {
+      // well, at least we tried ..
+    }
+    return DateTimeFormat.getLongDateFormat();
+  }
+  
 }
