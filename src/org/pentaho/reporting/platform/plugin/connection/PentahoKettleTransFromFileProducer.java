4cac924bbfda48dbab1b6eed5270f9ac874d655b
Thomas Morgner <tmorgner@pentaho.com>
2010-01-15 20:06:34 +0000

    PRD-2388: The crude and ugly way this time. We blindly translate the incoming path into a solution:/.. VFS path and hope that everything is alright. I have no clue how that stuff reacts to pure database-repositories, but as the VFS is provided by the platform, I'll blame them on errors.

--- src/org/pentaho/reporting/platform/plugin/connection/PentahoKettleTransFromFileProducer.java
+++ src/org/pentaho/reporting/platform/plugin/connection/PentahoKettleTransFromFileProducer.java
@@ -7,6 +7,7 @@ import org.apache.commons.vfs.VFS;
 import org.pentaho.reporting.engine.classic.core.ParameterMapping;
 import org.pentaho.reporting.engine.classic.extensions.datasources.kettle.KettleTransFromFileProducer;
 import org.pentaho.reporting.libraries.resourceloader.ResourceKey;
+import org.pentaho.reporting.platform.plugin.RepositoryResourceLoader;
 
 /**
  * Todo: Document me!
@@ -33,22 +34,36 @@ public class PentahoKettleTransFromFileProducer extends KettleTransFromFileProdu
   {
     while (key != null)
     {
+      final Object schema = key.getSchema();
+      if (RepositoryResourceLoader.SOLUTION_SCHEMA_NAME.equals(schema) == false)
+      {
+        // these are not the droids you are looking for ..
+        continue;
+      }
+
       final Object identifier = key.getIdentifier();
       if (identifier instanceof String)
       {
+        // get a local file reference ...
         try
         {
           final String file = (String) identifier;
           final FileSystemManager fileSystemManager = VFS.getManager();
+          final String[] schemes = fileSystemManager.getSchemes();
           // We try to create a file-object. If that works, we are talking about a VFS path and we can be happy.
           //noinspection UnusedDeclaration
-          final FileObject fileObject = fileSystemManager.resolveFile(file);
-          return file;
+          final FileObject fileObject = fileSystemManager.resolveFile("solution:/" + file);
+          if (fileObject.exists())
+          {
+            return "solution:/" + file;
+          }
         }
         catch (FileSystemException e)
         {
-          // ignored. 
+          // ignored.
+          e.printStackTrace();
         }
+
       }
       key = key.getParent();
     }
