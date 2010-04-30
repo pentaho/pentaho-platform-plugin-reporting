21674555dfd0eb745e732633089286deb4aa76af
Michael D'Aamour <mdamour@pentaho.com>
2010-04-30 18:45:00 +0000

    BISERVER-4213 Shading/Translucency of report execution results does not go away when report is done running until the user clicks around a few times.

--- src/org/pentaho/reporting/platform/plugin/gwt/client/ReportContainer.java
+++ src/org/pentaho/reporting/platform/plugin/gwt/client/ReportContainer.java
@@ -10,6 +10,7 @@ import org.pentaho.gwt.widgets.client.utils.string.StringUtils;
 import org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer.RENDER_TYPE;
 
 import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -49,6 +50,13 @@ public class ReportContainer extends VerticalPanel implements IParameterSubmissi
           WaitPopup.getInstance().setVisible(true);
         }
         super.setUrl(url);
+        // ie is not responding to onload
+        Timer t = new Timer() {
+          public void run() {
+            WaitPopup.getInstance().setVisible(false);
+          }
+        };
+        t.schedule(1000);
       }
       
     };
