233031efbfe725a465282a16e37a5feac8c5f53e
Marc Batchelor <mbatchelor@pentaho.com>
2010-05-03 15:38:38 +0000

    Backport of BISERVER-4079

--- src/org/pentaho/reporting/platform/plugin/PentahoReportEnvironment.java
+++ src/org/pentaho/reporting/platform/plugin/PentahoReportEnvironment.java
@@ -1,28 +1,41 @@
 package org.pentaho.reporting.platform.plugin;
 
 import java.net.URL;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Locale;
 
-import org.pentaho.platform.api.engine.IUserDetailsRoleListService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.security.SecurityHelper;
 import org.pentaho.platform.util.messages.LocaleHelper;
 import org.pentaho.reporting.engine.classic.core.DefaultReportEnvironment;
 import org.pentaho.reporting.engine.classic.core.modules.output.csv.CSVQuoter;
 import org.pentaho.reporting.libraries.base.config.Configuration;
 import org.pentaho.reporting.platform.plugin.messages.Messages;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
 
 public class PentahoReportEnvironment extends DefaultReportEnvironment
 {
   private static final Log logger = LogFactory.getLog(PentahoReportEnvironment.class);
+  private HashMap<String,String> cache;
 
   public PentahoReportEnvironment(final Configuration configuration)
   {
     super(configuration);
+  }
+
+  /**
+   * Returns the text encoding that should be used to encode URLs.
+   *
+   * @return the encoding for URLs.
+   */
+  public String getURLEncoding()
+  {
+    return LocaleHelper.getSystemEncoding();
   }
 
   public String getEnvironmentProperty(final String key)
@@ -32,25 +45,42 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
       throw new NullPointerException();
     }
 
+    if (cache == null)
+    {
+      cache = new HashMap<String,String>();
+    }
+    final String cached = cache.get(key);
+    if (cached != null)
+    {
+      return cached;
+    }
+    
     final IPentahoSession session = PentahoSessionHolder.getSession();
     if (PentahoSystem.getApplicationContext() != null)
     {
       final String pentahoBaseURL = PentahoSystem.getApplicationContext().getBaseUrl();
       if ("serverBaseURL".equals(key)) //$NON-NLS-1$
       {
-        return getBaseServerURL(pentahoBaseURL);
+        final String baseServerURL = getBaseServerURL(pentahoBaseURL);
+        cache.put(key, baseServerURL);
+        return baseServerURL;
       }
       else if ("pentahoBaseURL".equals(key)) //$NON-NLS-1$
       {
+        cache.put(key, pentahoBaseURL);
         return pentahoBaseURL;
       }
       else if ("solutionRoot".equals(key)) //$NON-NLS-1$
       {
-        return PentahoSystem.getApplicationContext().getSolutionPath(""); //$NON-NLS-1$
+        final String solutionRoot = PentahoSystem.getApplicationContext().getSolutionPath("");
+        cache.put(key, solutionRoot);
+        return solutionRoot; //$NON-NLS-1$
       }
       else if ("hostColonPort".equals(key)) //$NON-NLS-1$
       {
-        return getHostColonPort(pentahoBaseURL);
+        final String hostColonPort = getHostColonPort(pentahoBaseURL);
+        cache.put(key, hostColonPort);
+        return hostColonPort;
       }
     }
     else
@@ -64,6 +94,7 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
         // make it explicit that these values are not available. This way
         // a configuration in the classic-engine.properties file cannot begin
         // to interfer here.
+        cache.put(key, null);
         return null;
       }
     }
@@ -71,33 +102,32 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
     {
       if ("username".equals(key)) //$NON-NLS-1$
       {
-        return session.getName();
+        Authentication authentication = SecurityHelper.getAuthentication(session, true);
+        final String userName = authentication.getName();
+        cache.put(key, userName);
+        return userName;
       }
       else if ("roles".equals(key)) //$NON-NLS-1$
       {
-        final IUserDetailsRoleListService roleListService = PentahoSystem.getUserDetailsRoleListService();
-        if (roleListService == null)
-        {
-          return null;
-        }
+        Authentication authentication = SecurityHelper.getAuthentication(session, true);
         final StringBuffer property = new StringBuffer();
         //noinspection unchecked
-        final List<String> roles =
-            (List<String>) roleListService.getRolesForUser(session.getName());
+        final GrantedAuthority[] roles =
+            authentication.getAuthorities();
         if (roles == null)
         {
           return null;
         }
 
-        final int rolesSize = roles.size();
+        final int rolesSize = roles.length;
         if (rolesSize > 0)
         {
           final CSVQuoter quoter = new CSVQuoter(",");//$NON-NLS-1$
-          property.append(roles.get(0));
+          property.append(roles[0]);
           for (int i = 1; i < rolesSize; i++)
           {
             property.append(",");//$NON-NLS-1$
-            property.append(quoter.doQuoting(roles.get(i)));
+            property.append(quoter.doQuoting(roles[i].getAuthority()));
           }
         }
         return property.toString();
@@ -106,18 +136,14 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
       if (key.startsWith("session:"))//$NON-NLS-1$
       {
         final Object attribute = session.getAttribute(key.substring("session:".length()));//$NON-NLS-1$
-        if (attribute instanceof String)
-        {
-          return (String) attribute;
-        }
-        return null;
+        return String.valueOf(attribute);
       }
     }
     else
     {
-      if (key.startsWith("session:") ||
-          key.equals("username") ||
-          key.equals("roles"))
+      if (key.startsWith("session:") ||//$NON-NLS-1$
+          key.equals("username") ||//$NON-NLS-1$
+          key.equals("roles"))//$NON-NLS-1$
       {
         logger.warn(Messages.getString("ReportPlugin.warnNoSession"));
         return null;
@@ -136,7 +162,7 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
     }
     catch (Exception e)
     {
-      // ignore
+      // ignored
     }
     return pentahoBaseURL;
   }
@@ -150,7 +176,7 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
     }
     catch (Exception e)
     {
-      // ignore 
+      // ignored 
     }
     return pentahoBaseURL;
   }
@@ -158,10 +184,5 @@ public class PentahoReportEnvironment extends DefaultReportEnvironment
   public Locale getLocale()
   {
     return LocaleHelper.getLocale();
-  }
-
-  public String getURLEncoding()
-  {
-    return LocaleHelper.getSystemEncoding();
   }
 }
