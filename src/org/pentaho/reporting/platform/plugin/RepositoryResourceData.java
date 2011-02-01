0268e78c2f7379a1ffe55f99a219b94b851f6760
William Seyler <wseyler@pentaho.com>
2011-02-01 14:40:42 +0000

    BISERVER-5369: Fixed pathing issues to prevent errors when working with a file based solution.

--- src/org/pentaho/reporting/platform/plugin/RepositoryResourceData.java
+++ src/org/pentaho/reporting/platform/plugin/RepositoryResourceData.java
@@ -63,7 +63,11 @@ public class RepositoryResourceData extends AbstractResourceData {
   public InputStream getResourceAsStream(final ResourceManager caller) throws ResourceLoadingException {
     try {
       final ISolutionRepository solutionRepository = PentahoSystem.get(ISolutionRepository.class);
-      return solutionRepository.getResourceInputStream(key.getIdentifier().toString(), false, ISolutionRepository.ACTION_EXECUTE);
+      String fileIdentifier = key.getIdentifierAsString();
+      if (fileIdentifier.startsWith("/")) { // Remove the leading slash so this doesn't bum out a file based repository
+        fileIdentifier = fileIdentifier.substring(1);
+      }
+      return solutionRepository.getResourceInputStream(fileIdentifier, false, ISolutionRepository.ACTION_EXECUTE);
     } catch (FileNotFoundException e) {
       // might be due to access denial
       throw new ResourceLoadingException(e.getLocalizedMessage(), e);
