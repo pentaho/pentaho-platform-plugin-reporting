85648dcd009ce24ab7864c4c9bb7af754a1bf221
Will Gorman <wgorman@pentaho.com>
2011-03-10 02:05:22 +0000

    PRD-3107: Implemented platform ICacheManager implementation of Report Data Caching API.  This allows administrators to flush the entire segment of cache, while still maintaining session based cached tables.

--- src/org/pentaho/reporting/platform/plugin/cache/PentahoDataCache.java
+++ src/org/pentaho/reporting/platform/plugin/cache/PentahoDataCache.java
@@ -1,10 +1,18 @@
 package org.pentaho.reporting.platform.plugin.cache;
 
+import java.util.List;
+
 import javax.swing.table.TableModel;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pentaho.platform.api.engine.ICacheManager;
+import org.pentaho.platform.api.engine.ILogoutListener;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.cache.CachableTableModel;
 import org.pentaho.reporting.engine.classic.core.cache.DataCache;
 import org.pentaho.reporting.engine.classic.core.cache.DataCacheKey;
 import org.pentaho.reporting.engine.classic.core.cache.DataCacheManager;
@@ -15,79 +23,198 @@ import org.pentaho.reporting.engine.classic.core.cache.DataCacheManager;
  *
  * @author Thomas Morgner.
  */
-public class PentahoDataCache implements DataCache
-{
-  private static String SESSION_ATTRIBUTE = PentahoDataCache.class.getName() + "-DataCache";
+public class PentahoDataCache implements DataCache, ILogoutListener {
 
+  private static final Log log = LogFactory.getLog(PentahoDataCache.class);
+
+  private static final String CACHE_NAME = "report-dataset-cache";
+
+  private static class CompositeKey {
+    String sessionId;
+    DataCacheKey dataCacheKey;
+    
+    public CompositeKey(String sessionId, DataCacheKey dataCacheKey) {
+      this.sessionId = sessionId;
+      this.dataCacheKey = dataCacheKey;
+    }
+    
+    public boolean equals(Object o) {
+      if (this == o)
+      {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass())
+      {
+        return false;
+      }
+      final CompositeKey that = (CompositeKey) o;
+
+      if (!sessionId.equals(that.sessionId))
+      {
+        return false;
+      }
+      if (!dataCacheKey.equals(that.dataCacheKey))
+      {
+        return false;
+      }
+
+      return true;
+    }
+    
+    public int hashCode()
+    {
+      int result = sessionId.hashCode();
+      result = 31 * result + dataCacheKey.hashCode();
+      return result;
+    }
+  }
+  
   private static class PentahoDataCacheManager implements DataCacheManager
   {
+    private ICacheManager cacheManager;
+    
     private PentahoDataCacheManager()
     {
+      cacheManager = PentahoSystem.getCacheManager(null); // cache manager gets loaded just once...
     }
 
     public void clearAll()
     {
-      final IPentahoSession session = PentahoSessionHolder.getSession();
-      final DataCache dataCache = (DataCache) session.getAttribute(SESSION_ATTRIBUTE);
-      if (dataCache == null)
+      if (cacheManager != null)
       {
-        return;
+        cacheManager.clearRegionCache(CACHE_NAME);
       }
-
-      dataCache.getCacheManager().clearAll();
     }
 
     public void shutdown()
     {
-      final IPentahoSession session = PentahoSessionHolder.getSession();
-      final DataCache dataCache = (DataCache) session.getAttribute(SESSION_ATTRIBUTE);
-      if (dataCache == null)
+      if (cacheManager != null) 
       {
-        return;
+        cacheManager.removeRegionCache(CACHE_NAME);
       }
-
-      dataCache.getCacheManager().shutdown();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void killSessionCache(IPentahoSession session) 
+    {
+      if(cacheManager != null) 
+      {
+        List cachedObjects = cacheManager.getAllValuesFromRegionCache(CACHE_NAME);
+        if (cachedObjects != null) 
+        {
+          for (Object k : cachedObjects) 
+          {
+            if (k instanceof CompositeKey) 
+            {
+              CompositeKey key = (CompositeKey) k;
+              if(key.sessionId.equals(session.getId())) 
+              {
+                cacheManager.removeFromRegionCache(CACHE_NAME, key);
+              }
+            }
+          }
+        }
+      }
     }
   }
 
   private PentahoDataCacheManager manager;
-
+  private ICacheManager cacheManager;
+  private int maximumRows;
+  
   public PentahoDataCache()
   {
+    if (log.isDebugEnabled()) {
+      log.debug("Initializing");
+    }
+    maximumRows = ClassicEngineBoot.getInstance().getExtendedConfig().getIntProperty
+    ("org.pentaho.reporting.platform.plugin.cache.PentahoDataCache.CachableRowLimit");
+    
+    if (log.isDebugEnabled()) {
+      log.debug("Maximum Rows: " + maximumRows);
+    }
+    
+    cacheManager = PentahoSystem.getCacheManager(null); // cache manager gets loaded just once...
     manager = new PentahoDataCacheManager();
+    if (cacheManager != null) {
+      if (!cacheManager.cacheEnabled(CACHE_NAME)) {
+        if (!cacheManager.addCacheRegion(CACHE_NAME)) {
+          cacheManager = null;
+          manager.cacheManager = null;
+          throw new IllegalStateException("PentahoDataCache (" + CACHE_NAME + ") cannot be initialized");
+        }
+      }
+    }
+    
+    PentahoSystem.addLogoutListener(this); // So you can remove a users' region when their session disappears
   }
-
+  
   public synchronized TableModel get(final DataCacheKey key)
   {
-    final IPentahoSession session = PentahoSessionHolder.getSession();
-    final DataCache dataCache = (DataCache) session.getAttribute(SESSION_ATTRIBUTE);
-    if (dataCache == null)
+    if (cacheManager == null)
     {
       return null;
     }
+    
+    final IPentahoSession session = PentahoSessionHolder.getSession();
 
-    return dataCache.get(key);
+    if (log.isDebugEnabled()) {
+      log.debug("looking up key for session " + session.getId());
+    }
+
+    return (TableModel)cacheManager.getFromRegionCache(CACHE_NAME, new CompositeKey(session.getId(), key));
   }
 
+  
+  
   public synchronized TableModel put(final DataCacheKey key, final TableModel model)
   {
+    if (log.isDebugEnabled()) {
+      log.debug("put() called");
+    }
+    
     final IPentahoSession session = PentahoSessionHolder.getSession();
-    DataCache dataCache = (DataCache) session.getAttribute(SESSION_ATTRIBUTE);
-    if (dataCache == null)
+    if (cacheManager != null)
     {
-      dataCache = PentahoSystem.get(DataCache.class);
-      if (dataCache == null)
+      if (model.getRowCount() > maximumRows)
       {
-        // shield us from invalid spring configs
+        
+        if (log.isDebugEnabled()) {
+          log.debug("too many rows (" + model.getRowCount() + " > " + maximumRows +") not caching.");
+        }
         return model;
       }
-      session.setAttribute(SESSION_ATTRIBUTE, dataCache);
+      
+      // Only copy if safe to do so. Check for whitelist of good column types ..
+      if (CachableTableModel.isSafeToCache(model) == false)
+      {
+        if (log.isDebugEnabled()) {
+          log.debug("model is not safe to cache. not caching.");
+        }
+        return model;
+      }
+
+      if (log.isDebugEnabled()) {
+        log.debug("placing model in cache for session " + session.getId()+ " (rows=" + model.getColumnCount() + ")");
+      }
+      final TableModel cacheModel = new CachableTableModel(model);      
+      cacheManager.putInRegionCache(CACHE_NAME, new CompositeKey(session.getId(), key), cacheModel);
     }
-    return dataCache.put(key, model);
+    return model;
   }
 
   public DataCacheManager getCacheManager()
   {
     return manager;
+  }
+
+  @Override
+  public void onLogout(IPentahoSession session) {
+    
+    if (log.isDebugEnabled()) {
+      log.debug("killing session cache for " + session.getId());
+    }
+    manager.killSessionCache(session);
+    
   }
 }
