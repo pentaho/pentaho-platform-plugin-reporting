bb5669ae2fbfc5fa323b2c3722b44d9a0e87bb18
Michael D'Aamour <mdamour@pentaho.com>
2011-08-22 20:53:44 +0000

    BISERVER-6459 Cannot invoke url-dialog in report viewer more than twice in a row if contents of dialog are another report

--- src/org/pentaho/reporting/platform/plugin/gwt/client/ReportViewer.java
+++ src/org/pentaho/reporting/platform/plugin/gwt/client/ReportViewer.java
@@ -69,17 +69,17 @@ public class ReportViewer implements EntryPoint, IResourceBundleLoadCallback
   }
 
   private native void setupNativeHooks(ReportViewer viewer)
-    /*-{
-      $wnd.reportViewer_openUrlInDialog = function(title, message, width, height) {
-        viewer.@org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer::openUrlInDialog(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(title, message, width, height);
-      };
-      top.reportViewer_openUrlInDialog = function(title, message, width, height) {
-        viewer.@org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer::openUrlInDialog(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(title, message, width, height);
-      };
-      $wnd.reportViewer_hide = function() {
-        viewer.@org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer::hide()();
-      };
-    }-*/;
+  /*-{
+    if ($wnd.reportViewer_openUrlInDialog) {
+      return;
+    }
+    $wnd.reportViewer_openUrlInDialog = function(title, message, width, height) {
+      viewer.@org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer::openUrlInDialog(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(title, message, width, height);
+    };
+    $wnd.reportViewer_hide = function() {
+      viewer.@org.pentaho.reporting.platform.plugin.gwt.client.ReportViewer::hide()();
+    };
+  }-*/;
 
   private void hide()
   {
