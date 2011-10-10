9df300dd93c2749f6fc464f843bf45cf03f0057e
Jordan Ganoff <jganoff@pentaho.com>
2011-10-10 14:55:41 +0000

    [PIR-536] Major clean up and refactor of Prompting code. Made it possible to have multiple prompt panels on the same page, customize their behavior by overriding instanced methods, added beginnings of i18n support.

---
+++ package-res/resources/web/reportviewer-messages.js
@@ -0,0 +1,16 @@
+var ReportViewerMessages = function() {
+  this._bundle = {
+    'submitButtonText': 'View Report'
+  }
+
+  /**
+   * Get a string from a backing resource bundle.
+   * @param key Resource key to look up
+   * @param defaultString String to use if the key doesn't exist in the resource bundle.
+   * @param argArray Array of arguments for replacements.
+   */
+  this.getString = function(key, defaultString, argArray) {
+    // TODO Implement argArray replacement
+    return this._bundle[key] || defaultString || '!' + key + '!';
+  }
+}
