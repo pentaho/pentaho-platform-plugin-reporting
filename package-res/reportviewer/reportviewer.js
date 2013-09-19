279c29f80da45892dd522bcc7178de8e4613ac06
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-09-19 03:12:20 +0100

    * [PRD-4622] Showing Page 1/1 the first execution of paginated html report even when there are more than 1 pages. ** Restored INITIAL second call to "reporting" with renderMode=XML, and auto-submit=true. ** Restored a call to "reporting" with renderMode=XML, when on MANUAL and auto-submit=false. ** Made the glasspane survive on through multiple consecutive requests. * Fixed CSS position of off screen content (for size polling reasons). CSS measures did not have a unit specified, resulting in strange interactions with other css properties (namely: body { height:100% })

--- package-res/reportviewer/reportviewer.js
+++ package-res/reportviewer/reportviewer.js
@@ -213,7 +213,7 @@ pen.define(['common-ui/util/util','reportviewer/reportviewer-prompt', 'common-ui
         // Called by PromptPanel#postExecution (soon after initPrompt)
         promptReady: function(basePromptReady, promptPanel) {
 
-          basePromptReady();
+          basePromptReady(promptPanel); // hides the glass pane...
 
           if (inSchedulerDialog) {
             // If we are rendering parameters for the "New Schedule" dialog,
@@ -544,6 +544,7 @@ pen.define(['common-ui/util/util','reportviewer/reportviewer-prompt', 'common-ui
       },
       
       // Called by SubmitPromptComponent#expression (the submit button's click)
+      // Also may be called by PromptPanel#init, when there is no submit button (independently of autoSubmit?).
       submitReport: function(promptPanel, keyArgs) {
         var isInit = keyArgs && keyArgs.isInit;
         if(!isInit) {
@@ -577,7 +578,7 @@ pen.define(['common-ui/util/util','reportviewer/reportviewer-prompt', 'common-ui
             this._submitReportEnded(promptPanel);
             return;
           }
-        
+          
           this._updateReportContent(promptPanel, keyArgs);
         
         } catch(ex) {
@@ -591,18 +592,33 @@ pen.define(['common-ui/util/util','reportviewer/reportviewer-prompt', 'common-ui
       
       _updateReportContent: function(promptPanel, keyArgs) {
         var me = this;
-        
+
         // PRD-3962 - show glass pane on submit, hide when iframe is loaded
         // Show glass-pane
         dijit.byId('glassPane').show();
+        
+        if(!promptPanel.getAutoSubmitSetting()) {
+          // FETCH page-count info before rendering report
+          var callback = logged("_updateReportContent_fetchParameterCallback", function(newParamDefn) {
+            // Recreates the prompt panel's CDF components
+            promptPanel.refresh(newParamDefn);
+            
+            me._updateReportContentCore(promptPanel, keyArgs);
+          });
+
+          me.prompt.fetchParameterDefinition(promptPanel, callback, /*promptMode*/'MANUAL');
+        } else {
+          me._updateReportContentCore(promptPanel, keyArgs);
+        }
+      },
+
+      _updateReportContentCore: function(promptPanel, keyArgs) {
+        var me = this;
         
         // PRD-3962 - remove glass pane after 5 seconds in case iframe onload/onreadystatechange was not detected
         me._updateReportTimeout = setTimeout(logged('updateReportTimeout', function() {
           me._submitReportEnded(promptPanel, /*isTimeout*/true);
         }), 5000);
-        
-        // -----------
-        
         var options = me._buildReportContentOptions(promptPanel);
         var url = me._buildReportContentUrl(options);
         var outputFormat = options['output-target'];
