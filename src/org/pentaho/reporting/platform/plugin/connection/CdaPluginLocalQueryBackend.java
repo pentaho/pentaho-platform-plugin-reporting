dab01665009b84a19be691a1429f5ead18198f99
Pedro Vale <pedro.vale@webdetails.pt>
2013-10-10 18:09:38 +0100

    Fix for PRD-4767: CDA Datasource now working in Pentaho 5.0. CDA needs some refactoring to avoid the need to create HttpRequests and HttpResponses, but the fix will do for now

--- src/org/pentaho/reporting/platform/plugin/connection/CdaPluginLocalQueryBackend.java
+++ src/org/pentaho/reporting/platform/plugin/connection/CdaPluginLocalQueryBackend.java
@@ -17,21 +17,17 @@
 
 package org.pentaho.reporting.platform.plugin.connection;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URLDecoder;
+import java.security.Principal;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
-import org.pentaho.platform.api.engine.IContentGenerator;
-import org.pentaho.platform.api.engine.IOutputHandler;
 import org.pentaho.platform.api.engine.IParameterProvider;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.engine.IPluginManager;
-import org.pentaho.platform.engine.core.output.SimpleOutputHandler;
+
 import org.pentaho.platform.engine.core.solution.SimpleParameterProvider;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
@@ -40,6 +36,14 @@ import org.pentaho.reporting.engine.classic.core.ReportDataFactoryException;
 import org.pentaho.reporting.engine.classic.core.util.TypedTableModel;
 import org.pentaho.reporting.engine.classic.extensions.datasources.cda.CdaQueryBackend;
 import org.pentaho.reporting.engine.classic.extensions.datasources.cda.CdaResponseParser;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Class that implements CDA to be used LOCAL inside pentaho platform
@@ -84,28 +88,513 @@ public class CdaPluginLocalQueryBackend extends CdaQueryBackend {
 
     final IPentahoSession userSession = PentahoSessionHolder.getSession();
     final IPluginManager pluginManager = PentahoSystem.get( IPluginManager.class, userSession );
-    final IContentGenerator contentGenerator;
+
     try {
-      contentGenerator = pluginManager.getContentGeneratorForType( pluginName, userSession );
 
+
+      Object cdaBean = pluginManager.getBean("cda.api");
+      Class cdaBeanClass = cdaBean.getClass();
+
+      Class[] paramTypes;
+      Object[] paramValues;
+      Method m;
       final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-      final Map<String, Object> pathMap = new HashMap<String, Object>();
-      pathMap.put( "path", "/" + method );
-      final IParameterProvider pathParams = new SimpleParameterProvider( pathMap );
-      final Map<String, IParameterProvider> paramProvider = new HashMap<String, IParameterProvider>();
-      paramProvider.put( IParameterProvider.SCOPE_REQUEST, params );
-      paramProvider.put( "path", pathParams );
 
-      final IOutputHandler outputHandler = new SimpleOutputHandler( outputStream, false );
+      if ("listParameters".equals(method)) {
+        paramTypes = new Class[]{String.class,String.class,String.class,String.class,String.class,
+                HttpServletResponse.class, HttpServletRequest.class};
+        m =  cdaBeanClass.getMethod("listParameters", paramTypes);
+        paramValues = new Object[7];
+        paramValues[0] = URLDecoder.decode(params.getStringParameter("path", null), "UTF-8");
+        paramValues[1] = params.getStringParameter("solution", "");
+        paramValues[2] = params.getStringParameter("file", "");
+        paramValues[3] = params.getStringParameter("outputType", "json");
+        paramValues[4] = params.getStringParameter("dataAccessId", "<blank>");
+        paramValues[5] = getResponse(outputStream);
+        paramValues[6] = getRequest();
+      } else {
+        paramTypes = new Class[]{String.class, String.class, int.class,
+                String.class, Boolean.class, Boolean.class, int.class, int.class, Boolean.class,
+                List.class, HttpServletResponse.class, HttpServletRequest.class};
+        m = cdaBeanClass.getMethod("doQueryPost", paramTypes);
 
-      contentGenerator.setSession( userSession );
-      contentGenerator.setOutputHandler( outputHandler );
-      contentGenerator.setParameterProviders( paramProvider );
-      contentGenerator.createContent();
+        //Get parameters
+        paramValues = new Object[12];
+        paramValues[0] = URLDecoder.decode(params.getStringParameter("path", null), "UTF-8");
+        paramValues[1] = params.getStringParameter("outputType", "json");
+        paramValues[2] = (int) params.getLongParameter("outputIndexId", 1);
+        paramValues[3] = params.getStringParameter("dataAccessId", "1");
+        paramValues[4] = Boolean.parseBoolean(params.getStringParameter("bypassCache", "false"));
+        paramValues[5] = Boolean.parseBoolean(params.getStringParameter("paginateQuery", "false"));
+        paramValues[6] = (int) params.getLongParameter("pageSize", 0);
+        paramValues[7] = (int) params.getLongParameter("pageStart", 0);
+        paramValues[8] = Boolean.parseBoolean(params.getStringParameter("wrapItUp", "false"));
+
+        String[] sortFields = params.getStringArrayParameter("sortBy", new String[0]);
+        List<String> sortList = new ArrayList<String>(sortFields.length);
+        for (String sortField : sortFields) {
+          sortList.add(sortField);
+        }
+        paramValues[9] = sortList;
+        paramValues[10] = getResponse(outputStream);
+        paramValues[11] = getRequest();
+      }
+
+      m.invoke(cdaBean, paramValues);
       return outputStream.toString();
     } catch ( Exception e ) {
       throw new ReportDataFactoryException( "Failed to acquire " + pluginName + " plugin: ", e );
     }
   }
+
+
+  private static HttpServletRequest getRequest() {
+    return new HttpServletRequest() {
+      @Override
+      public String getAuthType() {
+        return null;
+      }
+
+      @Override
+      public Cookie[] getCookies() {
+        return new Cookie[0];
+      }
+
+      @Override
+      public long getDateHeader(String s) {
+        return 0;
+      }
+
+      @Override
+      public String getHeader(String s) {
+        return null;
+      }
+
+      @Override
+      public Enumeration getHeaders(String s) {
+        return null;
+      }
+
+      @Override
+      public Enumeration getHeaderNames() {
+        return null;
+      }
+
+      @Override
+      public int getIntHeader(String s) {
+        return 0;
+      }
+
+      @Override
+      public String getMethod() {
+        return null;
+      }
+
+      @Override
+      public String getPathInfo() {
+        return null;
+      }
+
+      @Override
+      public String getPathTranslated() {
+        return null;
+      }
+
+      @Override
+      public String getContextPath() {
+        return null;
+      }
+
+      @Override
+      public String getQueryString() {
+        return null;
+      }
+
+      @Override
+      public String getRemoteUser() {
+        return null;
+      }
+
+      @Override
+      public boolean isUserInRole(String s) {
+        return false;
+      }
+
+      @Override
+      public Principal getUserPrincipal() {
+        return null;
+      }
+
+      @Override
+      public String getRequestedSessionId() {
+        return null;
+      }
+
+      @Override
+      public String getRequestURI() {
+        return null;
+      }
+
+      @Override
+      public StringBuffer getRequestURL() {
+        return null;
+      }
+
+      @Override
+      public String getServletPath() {
+        return null;
+      }
+
+      @Override
+      public HttpSession getSession(boolean b) {
+        return null;
+      }
+
+      @Override
+      public HttpSession getSession() {
+        return null;
+      }
+
+      @Override
+      public boolean isRequestedSessionIdValid() {
+        return false;
+      }
+
+      @Override
+      public boolean isRequestedSessionIdFromCookie() {
+        return false;
+      }
+
+      @Override
+      public boolean isRequestedSessionIdFromURL() {
+        return false;
+      }
+
+      @Override
+      public boolean isRequestedSessionIdFromUrl() {
+        return false;
+      }
+
+      @Override
+      public Object getAttribute(String s) {
+        return null;
+      }
+
+      @Override
+      public Enumeration getAttributeNames() {
+        return null;
+      }
+
+      @Override
+      public String getCharacterEncoding() {
+        return null;
+      }
+
+      @Override
+      public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
+      }
+
+      @Override
+      public int getContentLength() {
+        return 0;
+      }
+
+      @Override
+      public String getContentType() {
+        return null;
+      }
+
+      @Override
+      public ServletInputStream getInputStream() throws IOException {
+        return null;
+      }
+
+      @Override
+      public String getParameter(String s) {
+        return null;
+      }
+
+      @Override
+      public Enumeration getParameterNames() {
+        return new Hashtable(0).elements();
+      }
+
+      @Override
+      public String[] getParameterValues(String s) {
+        return new String[0];
+      }
+
+      @Override
+      public Map getParameterMap() {
+        return null;
+      }
+
+      @Override
+      public String getProtocol() {
+        return null;
+      }
+
+      @Override
+      public String getScheme() {
+        return null;
+      }
+
+      @Override
+      public String getServerName() {
+        return null;
+      }
+
+      @Override
+      public int getServerPort() {
+        return 0;
+      }
+
+      @Override
+      public BufferedReader getReader() throws IOException {
+        return null;
+      }
+
+      @Override
+      public String getRemoteAddr() {
+        return null;
+      }
+
+      @Override
+      public String getRemoteHost() {
+        return null;
+      }
+
+      @Override
+      public void setAttribute(String s, Object o) {
+      }
+
+      @Override
+      public void removeAttribute(String s) {
+      }
+
+      @Override
+      public Locale getLocale() {
+        return null;
+      }
+
+      @Override
+      public Enumeration getLocales() {
+        return null;
+      }
+
+      @Override
+      public boolean isSecure() {
+        return false;
+      }
+
+      @Override
+      public RequestDispatcher getRequestDispatcher(String s) {
+        return null;
+      }
+
+      @Override
+      public String getRealPath(String s) {
+        return null;
+      }
+
+      @Override
+      public int getRemotePort() {
+        return 0;
+      }
+
+      @Override
+      public String getLocalName() {
+        return null;
+      }
+
+      @Override
+      public String getLocalAddr() {
+        return null;
+      }
+
+      @Override
+      public int getLocalPort() {
+        return 0;
+      }
+    }        ;
+  }
+
+
+  private static HttpServletResponse getResponse (final OutputStream stream) {
+    return new HttpServletResponse() {
+      @Override
+      public ServletOutputStream getOutputStream() throws IOException {
+        return new DelegatingServletOutputStream(stream);
+      }
+
+      //Needed to override but no implementation provided
+
+      @Override
+      public void addCookie(Cookie cookie) {
+      }
+
+      @Override
+      public boolean containsHeader(String s) {
+        return false;
+      }
+
+      @Override
+      public String encodeURL(String s) {
+        return null;
+      }
+
+      @Override
+      public String encodeRedirectURL(String s) {
+        return null;
+      }
+
+      @Override
+      public String encodeUrl(String s) {
+        return null;
+      }
+
+      @Override
+      public String encodeRedirectUrl(String s) {
+        return null;
+      }
+
+      @Override
+      public void sendError(int i, String s) throws IOException {
+      }
+
+      @Override
+      public void sendError(int i) throws IOException {
+      }
+
+      @Override
+      public void sendRedirect(String s) throws IOException {
+      }
+
+      @Override
+      public void setDateHeader(String s, long l) {
+      }
+
+      @Override
+      public void addDateHeader(String s, long l) {
+      }
+
+      @Override
+      public void setHeader(String s, String s2) {
+      }
+
+      @Override
+      public void addHeader(String s, String s2) {
+      }
+
+      @Override
+      public void setIntHeader(String s, int i) {
+      }
+
+      @Override
+      public void addIntHeader(String s, int i) {
+      }
+
+      @Override
+      public void setStatus(int i) {
+      }
+
+      @Override
+      public void setStatus(int i, String s) {
+
+      }
+
+      @Override
+      public String getCharacterEncoding() {
+        return null;
+      }
+
+      @Override
+      public String getContentType() {
+        return null;
+      }
+
+      @Override
+      public PrintWriter getWriter() throws IOException {
+        return null;
+      }
+
+      @Override
+      public void setCharacterEncoding(String s) {
+      }
+
+      @Override
+      public void setContentLength(int i) {
+      }
+
+      @Override
+      public void setContentType(String s) {
+      }
+
+      @Override
+      public void setBufferSize(int i) {
+      }
+
+      @Override
+      public int getBufferSize() {
+        return 0;
+      }
+
+      @Override
+      public void flushBuffer() throws IOException {
+      }
+
+      @Override
+      public void resetBuffer() {
+      }
+
+      @Override
+      public boolean isCommitted() {
+        return false;
+      }
+
+      @Override
+      public void reset() {
+      }
+
+      @Override
+      public void setLocale(Locale locale) {
+      }
+
+      @Override
+      public Locale getLocale() {
+        return null;
+      }
+    } ;
+  }
+
+
+
+  private static class DelegatingServletOutputStream extends ServletOutputStream {
+    private final OutputStream targetStream;
+    /**
+     * Create a new DelegatingServletOutputStream.
+     * @param targetStream the target OutputStream
+     */
+
+    public DelegatingServletOutputStream(OutputStream targetStream) {
+      this.targetStream = targetStream;
+    }
+
+
+    public void write(int b) throws IOException {
+      this.targetStream.write(b);
+    }
+
+    public void flush() throws IOException {
+      super.flush();
+      this.targetStream.flush();
+    }
+
+    public void close() throws IOException {
+      super.close();
+      this.targetStream.close();
+    }
+  }
+
 
 }
