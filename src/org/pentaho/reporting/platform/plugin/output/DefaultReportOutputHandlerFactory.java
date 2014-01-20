0d5b882ba748b76a51412a3e91e531b64c5fca01
Thomas Morgner <tmorgner@pentaho.com>
2014-01-20 14:28:05 +0000

    PRD-4921 - Fast export mode (bi-server test fixes)

--- src/org/pentaho/reporting/platform/plugin/output/DefaultReportOutputHandlerFactory.java
+++ src/org/pentaho/reporting/platform/plugin/output/DefaultReportOutputHandlerFactory.java
@@ -1,3 +1,19 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
 package org.pentaho.reporting.platform.plugin.output;
 
 import java.io.IOException;
@@ -52,6 +68,31 @@ public class DefaultReportOutputHandlerFactory implements ReportOutputHandlerFac
 
   public DefaultReportOutputHandlerFactory()
   {
+    htmlStreamVisible = true;
+    htmlPageVisible = true;
+    xlsVisible = true;
+    xlsxVisible = true;
+    csvVisible = true;
+    rtfVisible = true;
+    pdfVisible = true;
+    textVisible = true;
+    mailVisible = true;
+    xmlTableVisible = true;
+    xmlPageVisible = true;
+    pngVisible = true;
+
+    htmlStreamAvailable = true;
+    htmlPageAvailable = true;
+    xlsAvailable = true;
+    xlsxAvailable = true;
+    csvAvailable = true;
+    rtfAvailable = true;
+    pdfAvailable = true;
+    textAvailable = true;
+    mailAvailable = true;
+    xmlTableAvailable = true;
+    xmlPageAvailable = true;
+    pngAvailable = true;
   }
 
   public boolean isHtmlStreamVisible()
