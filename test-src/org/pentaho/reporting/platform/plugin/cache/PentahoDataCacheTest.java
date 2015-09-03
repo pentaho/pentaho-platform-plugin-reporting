d7d2fa80926ada7cc90cc40b297f8a423b522850
bhandy <brad.handy@gmail.com>
2015-09-03 08:02:16 -0400

    Resolve issue where the ScrollableResultSetTableModel is not closed after caching.  Leaving the underlying resultset open can lead to "maximum open cursors limit exceeded" errors in Oracle.

---
+++ test-src/org/pentaho/reporting/platform/plugin/cache/PentahoDataCacheTest.java
@@ -0,0 +1,69 @@
+package org.pentaho.reporting.platform.plugin.cache;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
+import org.pentaho.platform.engine.core.system.StandaloneSession;
+import org.pentaho.reporting.engine.classic.core.cache.DataCacheKey;
+import org.pentaho.reporting.platform.plugin.MockTableModel;
+
+import javax.swing.table.TableModel;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+
+public class PentahoDataCacheTest {
+
+    private PentahoDataCache dataCache;
+    private DataCacheKey dataCacheKey;
+    private MockTableModel tableModel;
+
+    @Before
+    public void setup() throws Exception {
+        createPentahoSession();
+        setupDataCache();
+
+        setupDataCacheKey();
+        setupTableModel();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        destroyPentahoSession();
+    }
+
+    @Test
+    public void verifyReturnTableModelShouldBeDifferentThanTheOriginalTableModel() {
+        assertNull("The data should not be cached.", dataCache.get(dataCacheKey));
+
+        TableModel cachedModel = dataCache.put(dataCacheKey, tableModel);
+
+        assertNotNull("The data should be cached.", dataCache.get(dataCacheKey));
+        assertNotSame("The original table model should not be returned.", tableModel, cachedModel);
+    }
+
+    private void createPentahoSession() {
+        PentahoSessionHolder.setSession(new StandaloneSession());
+    }
+
+    private void destroyPentahoSession() {
+        PentahoSessionHolder.setSession(null);
+    }
+
+    private void setupDataCache() {
+        dataCache = new PentahoDataCache();
+    }
+
+    private void setupDataCacheKey() {
+        dataCacheKey = new DataCacheKey();
+        dataCacheKey.addParameter("someParameter1", "someValue1");
+        dataCacheKey.addParameter("someParameter2", "someValue2");
+    }
+
+    private void setupTableModel() {
+        tableModel = new MockTableModel();
+    }
+
+}
