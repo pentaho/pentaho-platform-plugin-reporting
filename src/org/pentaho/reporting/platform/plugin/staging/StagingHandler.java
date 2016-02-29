8fc8d99908522b46c23829a456f701846bb5a52d
Dzmitry_Prakapenka <dima.prokopenko@gmail.com>
2016-02-29 14:19:53 +0300

    [BISERVER-1225] - As an end user when executing a report run, I want feedback on what is happening with the request.
    refactoring existing staging handlers.

---
+++ src/org/pentaho/reporting/platform/plugin/staging/StagingHandler.java
@@ -0,0 +1,54 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.reporting.platform.plugin.staging;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pentaho.platform.api.engine.IApplicationContext;
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.api.util.ITempFileDeleter;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.util.UUIDUtil;
+import org.pentaho.reporting.engine.classic.core.util.StagingMode;
+import org.pentaho.reporting.libraries.base.util.MemoryByteArrayOutputStream;
+
+public interface StagingHandler {
+
+  public StagingMode getStagingMode();
+
+  public boolean isFullyBuffered();
+
+  public boolean canSendHeaders();
+
+  public OutputStream getStagingOutputStream();
+
+  public void complete() throws IOException;
+
+  public void close();
+
+  public int getWrittenByteCount();
+}
