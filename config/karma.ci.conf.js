1aea79a69c89e6cc9e18b97a1fa9bc81dbed8aed
João Velasques <joao.velasques@webdetails.pt>
2016-03-09 16:03:55 +0000

    [BACKLOG-6961] - Setting up js test environment

---
+++ config/karma.ci.conf.js
@@ -0,0 +1,124 @@
+/*!
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+*/
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
+
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
+
+    // list of files / patterns to load in the browser
+    files: [
+
+      // CDF + Common-ui
+      {pattern: "build-res/module-scripts/**/*.+(js|html|xml)", included: false},
+
+      // Reporting
+      {pattern: "package-res/reportviewer/**/*.+(js|css)", included: false},
+      {pattern: 'test-js/**/*-spec.js', included: false},
+      {pattern: 'test-js/*.js', included: true},
+
+      'config/context.js',
+      'build-res/requireCfg-raw.js',
+      'config/require-config.js',
+
+      // fix 404 messages
+      {pattern: 'build-res/module-scripts/**/*.+(css|png|gif)', watched: false, included: false, served: true},
+    ],
+
+    // list of files to exclude
+    exclude: [
+      "build-res/module-scripts/reportviewer/*"
+    ],
+
+    preprocessors: {'package-res/reportviewer/**/*.js': 'coverage'},
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'junit', 'html', 'coverage'],
+
+    coverageReporter: {
+      type: 'cobertura',
+      dir: 'bin/test-reports/coverage/reports/'
+    },
+
+    junitReporter: {
+      outputFile: 'bin/test-reports/test-results.xml',
+      suite: 'unit'
+    },
+
+    // the default configuration
+    htmlReporter: {
+      outputDir:    'bin/test-reports/karma_html',
+      templatePath: 'node_modules/karma-html-reporter/jasmine_template.html'
+    },
+
+    //hostname
+    hostname: ['localhost'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // The configuration setting tells Karma how long to wait (in milliseconds) after any changes have occurred before starting the test process again.
+    //autoWatchBatchDelay: 250,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],//, 'Firefox', 'IE', 'PhantomJS'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // to avoid DISCONNECTED messages
+    // see https://github.com/karma-runner/karma/issues/598
+    browserDisconnectTimeout : 10000, // default 2000
+    browserDisconnectTolerance : 1, // default 0
+    browserNoActivityTimeout : 60000, //default 10000
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
+
+    plugins: [
+      'karma-jasmine',
+      'karma-requirejs',
+      'karma-junit-reporter',
+      'karma-html-reporter',
+      'karma-coverage',
+      'karma-phantomjs-launcher'
+    ]
+  });
+};
---
+++ config/require-config.js
@@ -0,0 +1,34 @@
+/*!
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+*/
+
+// Find and inject tests using require
+(function() {
+  var karma = window.__karma__;
+
+  var tests = [];
+  for(var file in karma.files) {
+    if((/test\-js.*\-spec\.js$/).test(file)) {
+      tests.push(file);
+    }
+  }
+
+  requireCfg['baseUrl'] = '/base';
+  requirejs.config(requireCfg);
+
+  // Ask Require.js to load all test files and start test run
+  require(tests, karma.start);
+})();
