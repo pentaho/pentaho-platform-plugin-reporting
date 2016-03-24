993615b764a285fbc6425d2786ae091b564c9d8e
Aliaksandr Kastenka <Aliaksandr_Kastenka@epam.com>
2016-03-24 11:34:08 +0300

    [BACKLOG-7404] Async mode still fires expensive pagination call

--- package-res/reportviewer/reportviewer-prompt.js
+++ package-res/reportviewer/reportviewer-prompt.js
@@ -99,7 +99,7 @@ define(['common-ui/util/util', 'pentaho/common/Messages', "dijit/registry", "com
             //
             // When paramDefn.allowAutoSubmit() is true,
             // And no validation errors/required parameters exist to be specified, TODO: Don't think that this is being checked here!
-            // Then a second request is made with promptMode='MANUAL' and renderMode='XML' is performed.
+            // In case when asynchronous mode is off - then a second request is made with promptMode='MANUAL' and renderMode='XML' is performed.
             //
             // When the response to the second request arrives,
             // Then the prompt panel is rendered, including with page count information, and  the report content is loaded and shown.
