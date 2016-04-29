022f991382e92dc5ecab80e02b8360ced5efecd8
Egor Zhuk <Yahor_Zhuk@epam.com>
2016-04-29 09:38:24 +0300

    [CLEANUP] - Fix FeedbackScreen.js format to fix build

--- package-res/reportviewer/dojo/pentaho/reportviewer/FeedbackScreen.js
+++ package-res/reportviewer/dojo/pentaho/reportviewer/FeedbackScreen.js
@@ -1,75 +1,76 @@
 /*!
-* Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
+ * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Templated", "dojo/on", "dojo/query",
-"pentaho/common/button", "pentaho/common/Dialog", "dojo/text!pentaho/reportviewer/FeedbackScreen.html"],
-    function(declare, _WidgetBase, _Templated, on, query, button, Dialog, templateStr){
+      "pentaho/common/button", "pentaho/common/Dialog", "dojo/text!pentaho/reportviewer/FeedbackScreen.html"],
+    function (declare, _WidgetBase, _Templated, on, query, button, Dialog, templateStr) {
       return declare("pentaho.reportviewer.FeedbackScreen", [Dialog],
-     {
-        buttons: ['cancel'],
+          {
+            buttons: ['cancel'],
 
-        imagePath: '',
+            imagePath: '',
 
-        hasTitleBar: false,
+            hasTitleBar: false,
 
-        setTitle: function(title) {
-            this.feedbacktitle.innerHTML = title;
-        },
+            setTitle: function (title) {
+              this.feedbacktitle.innerHTML = title;
+            },
 
-        setText: function(text) {
-            this.feedbackmessage.innerHTML = text;
-        },
+            setText: function (text) {
+              this.feedbackmessage.innerHTML = text;
+            },
 
-        setText2: function(text) {
-            this.feedbackmessage2.innerHTML = text;
-        },
+            setText2: function (text) {
+              this.feedbackmessage2.innerHTML = text;
+            },
 
-        setText3: function(text) {
-            this.feedbackmessage3.innerHTML = text;
-        },
+            setText3: function (text) {
+              this.feedbackmessage3.innerHTML = text;
+            },
 
-        setCancelText: function(text) {
-          var i = this.buttons.length > 1 ? 1 : 0;
-          this.buttons[i] = text;
-          query("#button"+i, this.domNode).forEach(function(node, index, arr){
-            node.innerHTML = text;
-          });
-        },
+            setCancelText: function (text) {
+              var i = this.buttons.length > 1 ? 1 : 0;
+              this.buttons[i] = text;
+              query("#button" + i, this.domNode).forEach(function (node, index, arr) {
+                node.innerHTML = text;
+              });
+            },
 
-       hideBackgroundBtn: function( ) {
-         if(this.buttons.length > 1 ){
-           this.buttons.shift();
-         }
-       },
+            hideBackgroundBtn: function () {
+              if (this.buttons.length > 1) {
+                this.buttons.shift();
+              }
+            },
 
-       showBackgroundBtn: function(text) {
-         if(this.buttons.length == 1 ){
-           this.buttons.unshift(text);
-           query("#button"+1, this.domNode).forEach(function(node, index, arr){
-             node.innerHTML = text;
-           });
-         }
-       },
+            showBackgroundBtn: function (text) {
+              if (this.buttons.length == 1) {
+                this.buttons.unshift(text);
+                query("#button" + 1, this.domNode).forEach(function (node, index, arr) {
+                  node.innerHTML = text;
+                });
+              }
+            },
 
-       templateString: templateStr,
+            templateString: templateStr,
 
-       postCreate: function() {
-           this.inherited(arguments);
-       }
+            postCreate: function () {
+              this.inherited(arguments);
+            }
 
-    }
-);
+          }
+      );
     });
+
