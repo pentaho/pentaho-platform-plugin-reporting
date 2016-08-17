179488d6b31f80f802664e043a0c4b559a0b3d50
Egor Zhuk <Yahor_Zhuk@epam.com>
2016-08-17 16:59:16 +0300

    [BACKLOG-10020] - Clear cache action

---
+++ test-src/org/pentaho/reporting/platform/plugin/async/ClearCacheActionTest.java
@@ -0,0 +1,43 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.reporting.platform.plugin.async;
+
+import org.junit.Test;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.reporting.platform.plugin.cache.IPluginCacheManager;
+import org.pentaho.reporting.platform.plugin.cache.IReportContentCache;
+import org.pentaho.reporting.platform.plugin.cache.PluginCacheManagerImpl;
+
+import static org.mockito.Mockito.*;
+
+public class ClearCacheActionTest {
+
+
+  @Test
+  public void runNoBean() {
+    new ClearCacheAction().run();
+  }
+
+  @Test
+  public void run() {
+    final IReportContentCache mock = mock( IReportContentCache.class );
+    PentahoSystem.registerObject( new PluginCacheManagerImpl( mock ), IPluginCacheManager.class );
+    new ClearCacheAction().run();
+    verify( mock, times( 1 ) ).cleanup();
+  }
+
+}
