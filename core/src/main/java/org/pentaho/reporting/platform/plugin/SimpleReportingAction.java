a5f91acd0f31722aa62c207f01fcb1450042e3d3
Sérgio Samora <sergiosamora@gmail.com>
2018-10-01 15:39:04 +0100

    [SP-4688] Backport of PIR-1414 - Report does not render complete data when Query Limit, Design Query limit are set and exported via Schedule/Run in Background (7.1 Suite)

--- core/src/main/java/org/pentaho/reporting/platform/plugin/SimpleReportingAction.java
+++ core/src/main/java/org/pentaho/reporting/platform/plugin/SimpleReportingAction.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2018 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.reporting.platform.plugin;
@@ -503,6 +503,7 @@ public class SimpleReportingAction implements IStreamProcessingAction, IStreamin
     } catch ( Throwable t ) {
       log.warn( t.getMessage(), t );
     }
+    report.setQueryLimit( -1 );
     return report;
   }
 
--- core/src/test/java/org/pentaho/reporting/platform/plugin/SimpleReportingActionTest.java
+++ core/src/test/java/org/pentaho/reporting/platform/plugin/SimpleReportingActionTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2018 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.reporting.platform.plugin;
@@ -198,6 +198,16 @@ public class SimpleReportingActionTest {
     sra.setVarArgs( inputs );
     sra.setReport( report );
     assertEquals( report, sra.getReport() );
+  }
+
+  @Test
+  public void testGetReportQueryLimit() throws Exception {
+
+    MasterReport report = new MasterReport();
+    report.setQueryLimit( 10 );
+    assertEquals( 10, report.getQueryLimit() );
+    sra.setReport( report );
+    assertEquals( -1, sra.getReport().getQueryLimit() );
   }
 
   @Test
