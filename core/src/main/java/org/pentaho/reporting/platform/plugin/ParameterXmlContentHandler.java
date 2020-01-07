aed6bcbe07ac2e158f5397529395294b71624005
David Griffen <david.griffen@hitachivantara.com>
2020-01-07 14:48:45 -0500

    [SP-5333] Backport of PRD-6038 - Auto-complete feature of Text Box is not working as expected when using a query to limit the results returned. (8.3 Suite)

--- core/src/main/java/org/pentaho/reporting/platform/plugin/ParameterXmlContentHandler.java
+++ core/src/main/java/org/pentaho/reporting/platform/plugin/ParameterXmlContentHandler.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2019 Hitachi Vantara..  All rights reserved.
+ * Copyright (c) 2002-2020 Hitachi Vantara..  All rights reserved.
  */
 
 package org.pentaho.reporting.platform.plugin;
@@ -604,7 +604,12 @@ public class ParameterXmlContentHandler {
       // then eliminate all downstream dependencies from this list
       if ( changedParameters.contains( knownParameter ) ) {
         for ( String child : dependencies.getDependentParameterFor( knownParameter ) ) {
-          effectivelyChanged.remove( child );
+          // [PRD-6038] A query parameter in the dependency graph may write the known parameter as the child parameter
+          // In this case, we need to make sure we don't remove the true changed parameter from the effectively changed
+          // parameter
+          if ( !child.equals( knownParameter ) ) {
+            effectivelyChanged.remove( child );
+          }
         }
       }
     }
